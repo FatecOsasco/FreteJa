c23540d4be6f48a5ce9301e430caac30c629a05e
Giovanni Nunes <133688956+giovanni-nunes04@users.noreply.github.com>
2025-09-22 01:19:08 -0300

    Update getters and setters from Proposta.java

--- src/main/java/com/freteja/model/Proposta.java
+++ src/main/java/com/freteja/model/Proposta.java
@@ -5,7 +5,9 @@ import java.time.Instant;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
 
+@Data
 @Document("propostas")
 public class Proposta {
   @Id
